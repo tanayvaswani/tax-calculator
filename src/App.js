cf0246aec619bdd35a35f3875a3a2e2a8edbbd84
tanayvaswani <vaswani.tanay9@gmail.com>
2023-06-17 19:19:28 +0530

    Tax calculator project.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [bill, setBill] = useState("");
+  const [tax, setTax] = useState(10);
+  const [quantity, setQuantity] = useState(1);
+  const [totalTax, setTotalTax] = useState(0);
+
+  function handleTaxModification(e) {
+    setBill(e.target.value);
+  }
+
+  function handleTaxChange(e) {
+    let value = e.target.value;
+    setTax(value);
+  }
+
+  function quantityInc() {
+    setQuantity((oldQuantity) => oldQuantity + 1);
+  }
+
+  function quantityDec() {
+    if (quantity > 0) {
+      setQuantity((oldQuantity) => oldQuantity - 1);
+    }
+  }
+
+  function finalTax() {
+    // const abc = tax.replace('%', '');
+    const percentage = tax / 100;
+    const result = (bill * quantity * percentage).toFixed(2);
+    setTotalTax(result);
+  }
+
+  useEffect(() => {
+    finalTax();
+  }, [bill, tax, quantity]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>Bill Total</label>
+      <input
+        type="text"
+        placeholder={"0.00"}
+        value={bill}
+        onChange={handleTaxModification}
+      />
+      <label>Tax</label>
+      <input
+        type="text"
+        placeholder={"0.00"}
+        value={tax}
+        onChange={handleTaxChange}
+      />
+      <div className="summary">
+        <div className="quantity">
+          <label>Quantity</label>
+          <div className="quantity-control">
+            <button onClick={quantityDec}>-</button>
+            <span>{quantity}</span>
+            <button onClick={quantityInc}>+</button>
+          </div>
+        </div>
+
+        <div className="result">
+          <label>Total Tax</label>
+          <span>{totalTax}</span>
+        </div>
+      </div>
     </div>
   );
 }
